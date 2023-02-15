b386884399ea6879b0d43e44ef7929dc2bdf0b47
bamidele emmanuel <emmanuelbam9876@gmail.com>
2023-02-15 14:06:07 -0800

    addedthe javascript json

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ let myLeads = [  ]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+
 inputBtn.addEventListener("click", function() {
    myLeads.push(inputEl.value)
    renderLeads()
@@ -12,7 +18,10 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a href='target=_'blank'>" + myLeads[i] + "</a></li>"
+        listItems += `<li>
+        <a target='_blank' href='${myLeads[i]}'>
+        ${myLeads[i]}</a>
+        </li>`
     }
     ulEl.innerHTML = listItems
 
